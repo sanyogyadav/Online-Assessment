68bf08d18ecfdda7c628b83699c809f457724b62
sanyogyadav <sanyogyadav52@gmail.com>
2022-12-13 00:38:19 +0530

    [SANYOG]-Basic Registration page is created.

---
+++ src/app/form-component/form-component.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-component',
+  templateUrl: './form-component.component.html',
+  styleUrls: ['./form-component.component.css']
+})
+export class FormComponentComponent {
+
+  submit(login : any){
+    console.log("Form Submitted",login)
+  }
+
+}
