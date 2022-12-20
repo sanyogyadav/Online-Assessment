3f81eae7b8cf998d840665903c21ab2962624e13
sanyogyadav <sanyogyadav52@gmail.com>
2022-12-20 17:12:53 +0530

    [SANYOG]-Changes in Online Component and CSS added to Form-Component

--- src/app/online-test/online-test.component.ts
+++ src/app/online-test/online-test.component.ts
@@ -13,8 +13,22 @@ export class OnlineTestComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  selectedValue: any;
+  type1: any;
+  type2: any;
+  type3: any;
+  type4: any;
+  type5: any;
+  type6: any;
+  type7: any;
+  type8: any;
+  type9: any;
+  type10: any;
+
   exampleFirst : string[] = ['Streams data in asynchronously','Streams data in synchronously','Both','None of above'];
+  exampleSecond : string[] = ['Object-Oriented','Object-Based','Assembly-language','High-level'];
+  exampleThird : string[] = ['Keywords','Data types','Declaration statements','Prototypes'];
+  exampleFourth : string[] = ['Global variable','The local element','The two of the above','None of the above'];
+  exampleFifth : string[] = ['Preprocessor','Triggering Event','RMI','Function/Method'];
 
   submit(online : any){
 
